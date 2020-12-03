f3cf88cbfa77ab4e440ff05983215e1fde78ebb8
guangjiu <zgjman@sina.com>
2020-12-03 13:39:15 +0800

    修改代码结构

---
+++ webrtclib/src/main/java/org/webrtc/NetworkPreference.java
@@ -0,0 +1,15 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package org.webrtc;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface NetworkPreference {
+    int NEUTRAL = 0;
+    int NOT_PREFERRED = -1;
+}
---
+++ webrtclib/src/main/java/org/webrtc/Priority.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package org.webrtc;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface Priority {
+    int VERY_LOW = 0;
+    int LOW = 1;
+    int MEDIUM = 2;
+    int HIGH = 3;
+}
